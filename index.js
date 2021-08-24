34d92fb68ac5d1dc1216de9e369642bdf4157501
Jeremiah <jsnidar@gmail.com>
2021-08-24 10:02:40 -0500

    commit tree 4b33804a9d6076fa715f5683688ff2c9407e82ee

--- index.js
+++ index.js
@@ -9,6 +9,56 @@
  for you to use if you need it!
  */
 
+ function createEmployeeRecord (array) {
+     return {firstName: array[0], familyName: array[1], title: array[2], payPerHour: array[3], timeInEvents: [], timeOutEvents: []}
+ }
+ 
+ function createEmployeeRecords (array) {
+    return array.map(employee => createEmployeeRecord(employee))
+ }
+
+ function createTimeInEvent (dateStamp) {
+
+    const timeInEventObj = {
+        type: 'TimeIn', 
+        hour: parseInt(dateStamp.substr(11, 4)) , 
+        date: dateStamp.substr(0,10)
+    }
+    const timeInArray = this.timeInEvents
+    timeInArray.push(timeInEventObj)
+    return this
+}
+
+function createTimeOutEvent (dateStamp) {
+    const timeOutEventObj = {
+        type: 'TimeOut', 
+        hour: parseInt(dateStamp.substr(11, 4)) , 
+        date: dateStamp.substr(0,10)
+    }
+    const timeOutArray = this.timeOutEvents
+    timeOutArray.push(timeOutEventObj)
+    return this
+}
+
+function hoursWorkedOnDate (dateStamp) {
+    const timeInArr = this.timeInEvents
+    const timeInObj = timeInArr.find(event => event.date === dateStamp.substr(0,10))
+    const timeOutArr = this.timeOutEvents
+    const timeOutObj = timeOutArr.find(event => event.date === dateStamp.substr(0,10))
+    return (timeOutObj.hour - timeInObj.hour)/100 
+}
+
+function wagesEarnedOnDate (dateStamp) {
+    const employeeWages = hoursWorkedOnDate.bind(this)
+    return employeeWages(dateStamp) * this.payPerHour
+}
+
+function findEmployeeByFirstName (srcArray, firstName) {
+    const match = srcArray.find(employee => employee.firstName === firstName)
+    return match;
+}
+
+
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -21,3 +71,8 @@ const allWagesFor = function () {
     return payable
 }
 
+function calculatePayroll (arrayOfEmployees) {
+    const allWages = arrayOfEmployees.map(employee => allWagesFor.call(employee))
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return allWages.reduce(reducer)
+}
